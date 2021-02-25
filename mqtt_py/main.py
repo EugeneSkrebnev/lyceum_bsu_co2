95480c7da900dfff36e9fa6209d17c2563e0a648
Skrebnev Eugene <Skrebnevchik@gmail.com>
2021-02-25 16:35:50 +0300

    add python test script for test values

---
+++ mqtt_py/main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+
+import paho.mqtt.client as mqtt
+from random import randint
+import time
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    client_name = 'test_values'
+    client = mqtt.Client(client_name)
+# put your mosquitto adr here
+    host = '192.168.1.84'
+    client.connect(host)
+    while True:
+        value = randint(0, 100)
+        client.publish("test/room/112", value)
+        time.sleep(3)
+
+
+
+
